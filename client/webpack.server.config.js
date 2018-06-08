b77d657428f21d459421fea8e81a1dae69b6247a
Trygve Sørensen <trygvsor@stud.ntnu.no>
2018-06-08 17:12:39 +0200

    Package updates, small fixes

--- client/webpack.server.config.js
+++ client/webpack.server.config.js
@@ -18,7 +18,7 @@ module.exports = {
 			{
 				// Mark files inside `@angular/core` as using SystemJS style dynamic imports.
 				// Removing this will cause deprecation warnings to appear.
-				test: /[\/\\]@angular[\/\\]core[\/\\].+\.js$/,
+				test: /(\\|\/)@angular(\\|\/)core(\\|\/).+\.js$/,
 				parser: { system: true }
 			}
 		]
