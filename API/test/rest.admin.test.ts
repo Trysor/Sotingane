22fdb6dc73d7c20b8a569044e36061b61d6f6f97
Trygve Sørensen <trygvsor@stud.ntnu.no>
2018-06-25 00:17:11 +0200

    API: Add 404 test to rest.admin.test, and reorder tests

--- API/test/rest.admin.test.ts
+++ API/test/rest.admin.test.ts
@@ -14,11 +14,12 @@ import { accessRoles } from '../src/models/user';
 describe('REST: Admin', () => {
 
 	// ---------------------------------
-	// ---------- /api/admin/ ----------
+	// ------- /api/admin/users --------
 	// ---------------------------------
 
 
 	describe('/api/admin/users/', () => {
+
 		it('GET /api/admin/users/ 200', async () => {
 			const res = await TestBed.http.get('/api/admin/users/').set('Authorization', TestBed.AdminToken);
 
@@ -44,7 +45,12 @@ describe('REST: Admin', () => {
 	});
 
 
+	// ---------------------------------
+	// ----- /api/admin/users/:id ------
+	// ---------------------------------
+
 	describe('/api/admin/users/:id', () => {
+
 		it('PATCH /api/admin/users/:id 200', async () => {
 			const patchUser = {
 				_id: TestBed.MemberUser.id,
@@ -59,7 +65,46 @@ describe('REST: Admin', () => {
 			expect(res).to.have.status(200);
 			expect(res.body).to.have.property('message');
 			expect(res.body.message).to.equal(USERS_STATUS.USER_UPDATED);
+		});
 
+		it('PATCH /api/admin/users/:id 400', async () => {
+			const patchUser = {
+				_id: TestBed.AdminUser.id, // Using AdminUser's ID instead
+				username: TestBed.MemberUser.username,
+				role: TestBed.MemberUser.role,
+			};
+
+			const res = await TestBed.http.patch('/api/admin/users/' + TestBed.MemberUser.id)
+				.set('Authorization', TestBed.AdminToken)
+				.send(patchUser);
+
+			expect(res).to.have.status(400);
+			expect(res.body).to.have.property('message');
+			expect(res.body.message).to.equal(USERS_STATUS.DATA_UNPROCESSABLE);
+		});
+
+		it('PATCH /api/admin/users/:id 401', async () => {
+
+			const patchUser = {
+				_id: TestBed.MemberUser.id,
+				username: TestBed.MemberUser.username,
+				role: TestBed.MemberUser.role,
+			};
+
+			const [noAuthRes, memberRes, adminRes] = await Promise.all([
+				TestBed.http.patch('/api/admin/users/' + TestBed.MemberUser.id)
+					.send(patchUser),
+				TestBed.http.patch('/api/admin/users/' + TestBed.MemberUser.id)
+					.set('Authorization', TestBed.MemberToken)
+					.send(patchUser),
+				TestBed.http.patch('/api/admin/users/' + TestBed.MemberUser.id)
+					.set('Authorization', TestBed.AdminToken)
+					.send(patchUser)
+			]);
+
+			expect(noAuthRes).to.have.status(401);
+			expect(memberRes).to.have.status(401);
+			expect(adminRes).to.have.status(200);
 		});
 
 		it('PATCH /api/admin/users/:id 409', async () => {
@@ -137,52 +182,16 @@ describe('REST: Admin', () => {
 			expect(tooManyRes.body.errors[0].params).to.have.property('additionalProperty');
 			expect(tooManyRes.body.errors[0].params.additionalProperty).to.equal('test');
 		});
-
-		it('PATCH /api/admin/users/:id 401', async () => {
-
-			const patchUser = {
-				_id: TestBed.MemberUser.id,
-				username: TestBed.MemberUser.username,
-				role: TestBed.MemberUser.role,
-			};
-
-			const [noAuthRes, memberRes, adminRes] = await Promise.all([
-				TestBed.http.patch('/api/admin/users/' + TestBed.MemberUser.id)
-					.send(patchUser),
-				TestBed.http.patch('/api/admin/users/' + TestBed.MemberUser.id)
-					.set('Authorization', TestBed.MemberToken)
-					.send(patchUser),
-				TestBed.http.patch('/api/admin/users/' + TestBed.MemberUser.id)
-					.set('Authorization', TestBed.AdminToken)
-					.send(patchUser)
-			]);
-
-			expect(noAuthRes).to.have.status(401);
-			expect(memberRes).to.have.status(401);
-			expect(adminRes).to.have.status(200);
-		});
-
-
-		it('PATCH /api/admin/users/:id 400', async () => {
-			const patchUser = {
-				_id: TestBed.AdminUser.id, // Using AdminUser's ID instead
-				username: TestBed.MemberUser.username,
-				role: TestBed.MemberUser.role,
-			};
-
-			const res = await TestBed.http.patch('/api/admin/users/' + TestBed.MemberUser.id)
-				.set('Authorization', TestBed.AdminToken)
-				.send(patchUser);
-
-			expect(res).to.have.status(400);
-			expect(res.body).to.have.property('message');
-			expect(res.body.message).to.equal(USERS_STATUS.DATA_UNPROCESSABLE);
-		});
 	});
 
 
+	// ---------------------------------
+	// -------- /api/admin/cms ---------
+	// ---------------------------------
+
 
 	describe('/api/admin/cms/', () => {
+
 		it('GET /api/admin/cms/ 200', async () => {
 
 			const res = await TestBed.http.get('/api/admin/cms/').set('Authorization', TestBed.AdminToken);
@@ -209,9 +218,13 @@ describe('REST: Admin', () => {
 		});
 	});
 
-	describe('/api/admin/cms/:route', () => {
-		it('GET /api/admin/cms/:route 200', async () => {
+	// ---------------------------------
+	// ----- /api/admin/cms/:route -----
+	// ---------------------------------
 
+	describe('/api/admin/cms/:route', () => {
+
+		it('GET /api/admin/cms/:route 200', async () => {
 			const content: Content = {
 				title: 'getadmin200',
 				route: 'getadmin200',
@@ -234,16 +247,6 @@ describe('REST: Admin', () => {
 			expect(res.body).to.have.property('published'); // admin only
 			expect(res.body).to.have.property('route');
 			expect(res.body.route).to.equal(content.route);
-		});
-
-		it('GET /api/admin/cms/:route 404', async () => {
-			const res = await TestBed.http.get('/api/admin/cms/' + 'some404route').set('Authorization', TestBed.AdminToken);
-
-			expect(res).to.have.status(404);
-			expect(res).to.have.property('body');
-			expect(res.body).to.have.property('message');
-			expect(res.body.message).to.equal(CMS_STATUS.CONTENT_NOT_FOUND);
-
 		});
 
 		it('GET /api/admin/cms/:route 401', async () => {
@@ -271,6 +274,15 @@ describe('REST: Admin', () => {
 			expect(noAuthRes).to.have.status(401);
 			expect(memberRes).to.have.status(401);
 			expect(adminRes).to.have.status(200);
+		});
+
+		it('GET /api/admin/cms/:route 404', async () => {
+			const res = await TestBed.http.get('/api/admin/cms/' + 'some404route').set('Authorization', TestBed.AdminToken);
+
+			expect(res).to.have.status(404);
+			expect(res).to.have.property('body');
+			expect(res.body).to.have.property('message');
+			expect(res.body.message).to.equal(CMS_STATUS.CONTENT_NOT_FOUND);
 		});
 	});
 
