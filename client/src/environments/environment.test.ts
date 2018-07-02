b3920aeb05dc12455cf891fed5acbaf6d0c73c48
Trysor <Trysor@users.noreply.github.com>
2018-07-02 23:02:43 +0200

    Fix test env

--- client/src/environments/environment.test.ts
+++ client/src/environments/environment.test.ts
@@ -1,5 +1,20 @@
-﻿
-import { env as devEnv } from './environment';
+import { API } from './api';
 
-export const env = Object.assign({}, devEnv);
-env.production = true;
+export const env = {
+	production: true,
+
+	TIMEOUT: 5000,
+
+	API: API,
+	API_BASE: '',
+
+	META: {
+		title: 'Test',
+		desc: 'Test desc',
+	},
+
+	FOOTER: {
+		desc: 'Tests',
+		copyright: 'test copyright'
+	}
+};
