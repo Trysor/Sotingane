e1079af5e35a710d314f54c5bda249c03b1d1e0e
Trygve Sørensen <trygvsor@stud.ntnu.no>
2018-07-12 21:16:39 +0200

    Client: change route animation setup

--- client/src/app/animations/routing.anim.ts
+++ client/src/app/animations/routing.anim.ts
@@ -1,21 +1,29 @@
-// import { trigger, animate, style, group, animateChild, query, stagger, transition } from '@angular/animations';
+import { trigger, animate, style, group, animateChild, query, stagger, transition, sequence } from '@angular/animations';
 
-// export const RoutingAnim = trigger('RoutingAnim', [
-  // transition('end => start', [ // Navigation occured, fade old out
-  //  query('.contentWrapper', [
-  //    style({ opacity: 1, }),
-  //    animate('1s ease-in-out', style({ opacity: 0 }))
-  //  ], { optional: true }),
-  // ]),
-  // transition('start => end', [ // Navigation ended, fade new item in
-  //  query('.contentWrapper', [
-  //    style({ opacity: 0.5, }),
-  //    animate('0.5s ease-in-out', style({ opacity: 1 }))
-  //  ], { optional: true }),
-  // ]),
-  // transition('end => start', [ // Navigation ended, fade new item in
-  //  query(':leave', [
-  //    style({ opacity: 0 }),
-  //  ], { optional: true }),
-  // ]),
-// ]);
+export const RoutingAnim = trigger('RoutingAnim', [
+    transition('* <=> *', [
+        group([
+            query(
+                ':leave',
+                [animate('0.35s', style({ opacity: 0 })), animateChild()],
+                { optional: true }
+            ),
+            query(
+                ':enter',
+                [
+                    style({
+                        opacity: 0,
+                        transform: 'translateY(-10%)'
+                    }),
+                    animate(
+                        '0.35s ease-in-out',
+                        style({ opacity: 1, transform: 'translateY(0)' })
+                    ),
+                    animateChild()
+                ],
+                { optional: true }
+            ),
+
+        ])
+    ])
+]);
