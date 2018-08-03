cf8c9beab3370da860200fdf3d0f069455f4ae3d
Trygve Sørensen <trygvsor@stud.ntnu.no>
2018-08-03 23:14:40 +0200

    Client: catch API errors

--- client/src/app/services/http/interceptor.service.ts
+++ client/src/app/services/http/interceptor.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+
+import { MatSnackBar } from '@angular/material';
 
 import { env } from '@env';
 
 import { TokenService } from '@app/services/utility/token.service';
 import { LoadingService } from '@app/services/utility/loading.service';
 
-import { Observable } from 'rxjs';
-import { timeout, finalize } from 'rxjs/operators';
+import { Observable, of, throwError, TimeoutError } from 'rxjs';
+import { timeout, finalize, catchError } from 'rxjs/operators';
 
 
 @Injectable()
@@ -15,7 +17,8 @@ export class InterceptorService implements HttpInterceptor {
 
 	constructor(
 		private tokenService: TokenService,
-		private loadingService: LoadingService) {
+		private loadingService: LoadingService,
+		private snackBar: MatSnackBar) {
 	}
 
 	intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -32,7 +35,27 @@ export class InterceptorService implements HttpInterceptor {
 		// Send it off to the next handle
 		return next.handle(req.clone({ headers: headers, withCredentials: true })).pipe(
 			timeout(env.TIMEOUT), // Set global TIMEOUT for all API calls
-			finalize(() => this.loadingService.removeRequest())
+			finalize(() => this.loadingService.removeRequest()),
+			catchError((err: HttpErrorResponse | TimeoutError, caught) => {
+				if (err instanceof TimeoutError) {
+					this.openSnackBar('Request timed out');
+					return throwError('Request timed out');
+				}
+				// Any non-timed-out request has to be handled from where it originated
+				return throwError(err.message);
+			})
 		);
+	}
+
+
+	/**
+	 * Opens a snackbar with the given message and action message
+	 * @param  {string} message The message that is to be displayed
+	 * @param  {string} action  the action message that is to be displayed
+	 */
+	private openSnackBar(message: string, action?: string) {
+		this.snackBar.open(message, action, {
+			duration: 5000,
+		});
 	}
 }
