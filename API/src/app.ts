5c59eee91f0d66d67d306e0b78262e9700499c13
Trygve Sørensen <trygvsor@stud.ntnu.no>
2018-09-13 23:00:15 +0200

    API: Add Mongoose global settings to move away from deprecated methods

--- API/src/app.ts
+++ API/src/app.ts
@@ -40,7 +40,13 @@ class App {
 
 		const uri = process.env.db || configGet<string>('database');
 
-		mongoose.connect(uri, { keepAlive: 120, useNewUrlParser: true }, (error) => {
+		mongoose.set('useCreateIndex', true);
+		mongoose.set('useFindAndModify', false);
+
+		mongoose.connect(uri, {
+			keepAlive: 120,
+			useNewUrlParser: true
+		}, (error) => {
 			if (error) {
 				// if error is true, the problem is often with mongoDB not connection
 				console.error('Mongoose connection error:', error.message);
