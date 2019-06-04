85aa83df042769eae3cfb23109d679a287bfa067
Trygve Sørensen <trygvsor@stud.ntnu.no>
2019-06-04 19:17:47 +0200

    Client: Update date-fns

--- client/src/app/pipes/time.ago.pipe.ts
+++ client/src/app/pipes/time.ago.pipe.ts
@@ -1,10 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { formatDistance } from 'date-fns';
+import { formatDistance, parseISO } from 'date-fns';
 
 @Pipe({ name: 'TimeAgo', pure: false }) // new Date() makes it unpure
 export class TimeAgoPipe implements PipeTransform {
-
 	transform(date: string | number | Date): string {
+		if (typeof date === 'string') {
+			date = parseISO(date);
+		}
 		if (!date) { throw new Error('timeAgo Pipe: Missing arg'); }
 		return formatDistance(date, new Date(), { addSuffix: true });
 	}
